6001620d7d44f1d8ea4e8d8025dea248ba715023
plon-Susk7 <priyashshah4@gmail.com>
2024-04-20 19:55:44 +0530

    basic socket code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import WebSocket,{WebSocketServer} from 'ws';
+import http from 'http';
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+
 const port = process.env.PORT || 5000;
 
+wss.on('connection',(ws:WebSocket)=>{
+    
+    ws.on('error',(err:Error)=>{console.log(err)});
+
+    ws.on('message',(message:String)=>{
+        console.log(`[server]: Received message => ${message}`);
+    })
+
+    ws.send('Hello World! Welcome to the websocket server');
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
